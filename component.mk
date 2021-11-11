21c46f33c4246ddd4d3023df45f64f2177883f28
mikee47 <mike@sillyhouse.net>
2021-11-11 11:16:13 +0000

    Rearrange/tidy source tree

--- component.mk
+++ component.mk
@@ -1,27 +1,16 @@
-COMPONENT_INCDIRS := include/
+COMPONENT_INCDIRS := \
+	bpf/include
 
-COMPONENT_CFLAGS := \
-	-Wno-format-nonliteral \
-	-Wno-override-init \
-	-Wno-pedantic
-
-COMPONENT_SRCFILES := \
-	src/bpf/bpf.c \
-	src/bpf/call.c \
-	src/bpf/store.c \
-	src/bpf/verify.c \
-	\
-	src/memarray/memarray.c \
-	\
-	src/btree/btree.c
-
-COMPONENT_CONFIG_VARS := BPF_USE_JUMPTABLE
+COMPONENT_RELINK_VARS := BPF_USE_JUMPTABLE
 BPF_USE_JUMPTABLE := 1
 
-ifeq ($(BPF_USE_JUMPTABLE), 1)
-  COMPONENT_SRCFILES += src/bpf/jumptable.c
+COMPONENT_SRCDIRS := \
+	bpf
+
+ifeq ($(BPF_USE_JUMPTABLE),1)
+  COMPONENT_SRCFILES += bpf/option/jumptable.c
 else
-  COMPONENT_SRCFILES += src/bpf/instruction.c
+  COMPONENT_SRCFILES += bpf/option/instruction.c
 endif
 
 RBPF_COMPONENT_PATH := $(COMPONENT_PATH)
@@ -36,17 +25,17 @@ export RBPF_BLOBDIR	:= $(PROJECT_DIR)/out/rbpf/obj
 export RBPF_INCDIR	:= $(PROJECT_DIR)/out/rbpf/include
 COMPONENT_INCDIRS	+= $(RBPF_INCDIR)
 
-RBPF_MAKE := $(MAKE) -C $(RBPF_CONTAINER_PATH) --no-print-directory -f $(RBPF_COMPONENT_PATH)/rbpf.inc.mk
+RBPF_MAKE = $(MAKE) -C $(RBPF_CONTAINER_PATH) --no-print-directory -f $(RBPF_COMPONENT_PATH)/rbpf.inc.mk
 
 .PHONY: rbpf-blobs
 rbpf-blobs: | $(RBPF_CONTAINER_PATH) $(RBPF_BLOBDIR) ##Compile container objects
 	$(Q) $(RBPF_MAKE) blobs
-	
+
 .PHONY: rbpf-blobs-clean
 rbpf-blobs-clean: ##Remove generated rBPF files
 	$(Q) rm -rf $(RBPF_BLOBDIR)
 	$(Q) $(RBPF_MAKE) clean
-	
+
 .PHONY: rbpf-blobs-dump
 rbpf-blobs-dump: ##Show container application
 	$(Q) $(RBPF_MAKE) dump
