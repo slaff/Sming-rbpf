54980bb6543172b786534e66ae33f388da632d2d
mikee47 <mike@sillyhouse.net>
2021-11-15 10:42:54 +0000

    Use jumptables implementation only

--- samples/Basic_VM/container/increment.c
+++ samples/Basic_VM/container/increment.c
@@ -1,21 +1,6 @@
 #include "include/container/increment.h"
-// #include <string.h>
-
-static int func1(void* x)
-{
-	(void)x;
-	return 100;
-}
 
 int increment(increment_context_t* context)
 {
-	// 	memset(context, 0, sizeof(increment_context_t) + 513); // Should go bang!!!
-	// return context->value + 1;
-	return func1(context);
-}
-
-int func2(void* x)
-{
-	(void)x;
-	return 200;
+	return context->value + 1;
 }
